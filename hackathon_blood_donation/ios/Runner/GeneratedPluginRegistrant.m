5fbd120ea1ddf2cd1edc4e58abed89a02c34a0e0
Mr.Phonpasit Bounkham <newbig6622@gmail.com>
2023-11-17 23:47:48 +0700

    set up multi languages complted

--- hackathon_blood_donation/ios/Runner/GeneratedPluginRegistrant.m
+++ hackathon_blood_donation/ios/Runner/GeneratedPluginRegistrant.m
@@ -18,11 +18,18 @@
 @import rive_common;
 #endif
 
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
   [RivePlugin registerWithRegistrar:[registry registrarForPlugin:@"RivePlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
 }
 
 @end

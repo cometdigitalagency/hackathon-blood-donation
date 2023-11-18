bf16c4d1402f26a82b3a5c258515266ca3dafc63
Mr.Phonpasit Bounkham <newbig6622@gmail.com>
2023-11-18 14:18:48 +0700

    update home UI
    add constants and add images

---
+++ hackathon_blood_donation/lib/app/constants/font_auto_size.dart
@@ -0,0 +1,8 @@
+class FontAutoSize {
+  // static double default_super_title_size = 0.025;
+  static double default_title_size = 0.02;
+  static double default_sub_title_size = 0.017;
+  // static double default_super_des_size = 0.015;
+  static double default_des_size = 0.012;
+  static double default_sub_des_size = 0.01;
+}
